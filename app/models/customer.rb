0ced400b2c17faa2538a60779fe6c63ede1ba54e
EC2 Default User <ec2-user@ip-172-31-0-185.ap-northeast-1.compute.internal>
2020-11-19 05:18:53 +0000

    修正しました。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,4 +13,10 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
+
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+
+
 end
