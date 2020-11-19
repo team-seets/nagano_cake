594ddd74695acb1a01221b7cd78eadf71d1d0f80
EC2 Default User <ec2-user@ip-172-31-94-209.ec2.internal>
2020-11-19 00:47:41 +0000

    admin側controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   resources :orders, only: [:new, :create, :index, :show, :thanks, :complete]
   resources :cart_products, only: [:index,:create,:update,:destroy,:destroy_all]
   resources :customers, only:[:index, :show, :edit, :create, :update, :destroy]
+  
+  namespace :admins do
+    resources :customers, only:[:top, :create, :destroy]
+    resources :items, only:[:index, :new, :show, :edit, :create, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :end_user, only: [:index, :show, :edit, :update]
+  end
 end
