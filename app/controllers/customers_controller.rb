21e6cd37d86f9d8b22606f60d2274f2e3fe6253f
EC2 Default User <ec2-user@ip-172-31-94-209.ec2.internal>
2020-11-21 09:08:48 +0000

    admins/products

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,38 +1,17 @@
 class CustomersController < ApplicationController
-<<<<<<< HEAD
-  
-  def index
-     @user = User.find(params[:id])
-  end
-
-  def show
-    # @user = User.find(params[:id])
-=======
   before_action :authenticate_customer!
 
   def show
     @customer = Customer.find(params[:id])
->>>>>>> fee0116a4afc191c4db8b595daf2e64de7a906ef
   end
 
   def edit
      @customer = Customer.find(params[:id])
-<<<<<<< HEAD
-    # if current_user.id != @user.user_id
-      # redirect_to customer_path(current_user.id)
-    # end
-  end
-
-  def update
-     @user = User.find(params[:id])
-    if @user.save(user_params)
-=======
   end
 
   def update
      @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
->>>>>>> fee0116a4afc191c4db8b595daf2e64de7a906ef
       redirect_to customer_path, notice: ''
     else
       render 'edit'
@@ -40,20 +19,6 @@ class CustomersController < ApplicationController
   end
 
   def destroy
-<<<<<<< HEAD
-    @user = User.find(params[:id])
-    @user.destroy
-    flash[:success] = 'ユーザーを削除しました。'
-    redirect_to root
-  end
-
- private
-    def user_params
-       params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
-    end
-
-end
-=======
     @customer = Customer.find(params[:id])
     @customer.destroy
     flash[:success] = 'ユーザーを削除しました。'
@@ -64,6 +29,4 @@ end
     def customer_params
        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
     end
-
 end
->>>>>>> fee0116a4afc191c4db8b595daf2e64de7a906ef
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_11_19_080610) do
-=======
-
 ActiveRecord::Schema.define(version: 2020_11_20_071854) do
->>>>>>> fee0116a4afc191c4db8b595daf2e64de7a906ef
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_11_20_071854) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.boolean "is_deleted", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -54,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_071854) do
     t.string "telephone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
