9b41bcec3a963ad487ffd40a87f21e133a07a5f1
EC2 Default User <ec2-user@ip-172-31-0-185.ap-northeast-1.compute.internal>
2020-11-21 05:52:02 +0000

    end_user_view追加

--- db/migrate/20201116152450_add_is_deleted_to_customers.rb
+++ db/migrate/20201116152450_add_is_deleted_to_customers.rb
@@ -1,5 +1,5 @@
 class AddIsDeletedToCustomers < ActiveRecord::Migration[5.2]
   def change
-   change_column_default :customers, :is_deleted, :boolean,　default: false
+   change_column_default :customers, :is_deleted, :boolean, default: false
   end
 end
