8e51b80e94e8aa36ce4420d45458fc8379644f1c
EC2 Default User <ec2-user@ip-172-31-0-185.ap-northeast-1.compute.internal>
2020-11-21 10:28:02 +0000

    修正しました。

--- app/controllers/admins/end_users_controller.rb
+++ app/controllers/admins/end_users_controller.rb
@@ -1,2 +1,29 @@
 class Admins::EndUsersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_end_user_path, notice: ''
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    end
 end
