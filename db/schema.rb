75f7d6ca859cc711a940f32cff67ed5573b2d6f9
EC2 Default User <ec2-user@ip-172-31-33-152.ec2.internal>
2020-11-21 05:50:00 +0000

    adminログイン

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_11_19_113207) do
 
+ActiveRecord::Schema.define(version: 2020_11_20_071854) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_113207) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "is_deleted", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -48,16 +49,13 @@ ActiveRecord::Schema.define(version: 2020_11_19_113207) do
     t.string "telephone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_deleted"
-    t.string "email"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_active" #有効ステータス
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
