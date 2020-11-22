a83af911a706cae92cbc8a97199b2967d18f976f
EC2 Default User <ec2-user@ip-172-31-0-185.ap-northeast-1.compute.internal>
2020-11-22 07:12:20 +0000

    修正しました2

--- app/controllers/admins/end_users_controller.rb
+++ app/controllers/admins/end_users_controller.rb
@@ -24,6 +24,6 @@ class Admins::EndUsersController < ApplicationController
 
   private
     def customer_params
-      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-    keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :address,:telephone_number ])
+    keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address,:telephone_number ])
 
     devise_parameter_sanitizer.permit(:account_update,
-    keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :address,:telephone_number ])
+    keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address,:telephone_number ])
   end
 end
