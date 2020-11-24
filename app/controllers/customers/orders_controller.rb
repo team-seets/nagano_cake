dffa4f1f25ffd25cd680776181bef58bb5553934
EC2 Default User <ec2-user@ip-172-31-33-152.ec2.internal>
2020-11-24 12:36:58 +0000

    1

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,31 +1,23 @@
 class Customers::OrdersController < ApplicationController
   def new
     @order = Order.new
+    
   end
 
   def create
-    binding.pry
-    @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
-    @order.save
-    redirect_to customers_complete_path
+    # @order = Order.new(order_params)
+    # @order.customer_id = current_customer.id
+    # @order.save
+    # redirect_to customers_complete_path
   end
 
   def complete
     @cart_products = current_customer.cart_products
     @totalprice = @cart_products.map{|cart_product|cart_product.product.price * cart_product.quantity}.inject(:+)
-    @order = Order.new
-    @order.payment_methods = params[:order][:payment_methods]
-    if params[:order][:select] == "1"
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.last_name + current_customer.first_name
-    # elsif 
-    else
-    @order.postal_code = params[:order][:postal_code]
-    @order.address = params[:order][:address]
+    @order = params[:order][:payment_methods]
+    @postal_code = params[:order][:postal_code]
+    @address = params[:order][:address]
     @name = params[:order][:name]
-    end
   end
 
   def thanx
