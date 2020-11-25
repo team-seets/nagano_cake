ebb9ad05ec1b38397dda0a21103d15b8ff20da14
EC2 Default User <ec2-user@ip-172-31-33-152.ec2.internal>
2020-11-25 08:13:06 +0000

    制作ステータスupdate

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,8 @@
 class Admins::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @cart_products = CartProduct.all
+    #@order_details = OrderDetails.all
+		#@order_detail = params[:making_status]
   end
 
   def index
@@ -12,15 +13,17 @@ class Admins::OrdersController < ApplicationController
   end
 
   def update
-
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admins_customer_path(@order)
   end
 
   private
   def order_params
-    params.require(:order).permit(:status, :name)
+    params.require(:order).permit(:status)
   end
 
   def order_details_params
-    params.require(:order_details).permit(:amount)
+    params.require(:order_details).permit(:amount, :making_status)
   end
 end
