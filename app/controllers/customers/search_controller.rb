c77313fc8785e1d9f98a8199b9ee1ace39ae022e
EC2 Default User <ec2-user@ip-172-31-94-209.ec2.internal>
2020-11-26 13:13:27 +0000

    kaminari追加しています

--- app/controllers/customers/search_controller.rb
+++ app/controllers/customers/search_controller.rb
@@ -14,19 +14,19 @@ class Customers::SearchController < ApplicationController
 	end
 	
   def products
-    @value = params["search"]["value"]         #データを代入
-    @how = params["search"]["how"]             #データを代入
-    @datas = search_for(@how, @value)          #def search_forを実行(引数に検索ワードと検索方法)
+    @value = params["search"]["value"]        
+    @how = params["search"]["how"]            
+    @datas = search_for(@how, @value).page(params[:page]).reverse_order         
   end
 
   private
 
   def match(value)
-    #.orを使用することで、valueに一致するカラムのデータをnameカラムとgenre_idカラムから探してきます。
+   
     Product.where(name: value).or(Product.where(genre_id: value))
   end
 
-  def forward(value)                              #forward以降は商品名検索の定義しかしてません。
+  def forward(value)                              
     Product.where("name LIKE ?", "#{value}%")        
   end
 
@@ -40,8 +40,8 @@ class Customers::SearchController < ApplicationController
 
 
   def search_for(how, value)
-    case how                     #引数のhowと一致する処理に進むように定義しています。
-    when 'match'                 #ジャンル検索の場合はmatchで固定してるので、必ず'match'の処理に進みます。
+    case how                     
+    when 'match'                 
       match(value)
     when 'forward'
       forward(value)
